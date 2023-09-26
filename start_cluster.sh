b00c610132c24695c4895ce72cdf5b5bed4f0d4b
tudv <dovantu29012000@gmail.com>
2023-09-26 20:17:59 +0700

    tudv upocode

---
+++ start_cluster.sh
@@ -0,0 +1,31 @@
+# #!/bin/bash
+
+# docker network create hadoop_network
+
+# docker build -t hadoop-base:3.3.1 .
+
+# docker-compose up -d
+
+#!/bin/bash
+
+# create new network
+docker network create hadoop_network
+
+# build docker image with image name hadoop-base:3.3.1
+docker build -t hadoop-base:3.3.1 -f Dockerfile-hadoop .
+# running image to container, -d to run it in daemon mode
+docker-compose -f docker-compose-hadoop.yml up
+
+# Run Airflow Cluster
+if [[ "$PWD" != "airflow" ]]; then
+  cd airflow && ./run_airflow.sh && cd ..
+fi
+
+docker-compose -f docker-compose-airflow.yml up
+
+# Run Spark Cluster
+if [[ "$PWD" != "spark" ]]; then
+  cd spark && ./start-cluster.sh && cd ..
+fi
+
+echo "Current dir is $PWD"
